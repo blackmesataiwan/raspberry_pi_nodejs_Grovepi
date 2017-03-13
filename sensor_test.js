caec0558cd2e2f3353ffad9b7c7e36b432888467
blackmesataiwan <san82718@gmail.com>
2017-03-13 19:28:56 +0800

    add sensor

--- sensor_test.js
+++ sensor_test.js
@@ -3,7 +3,10 @@ var GrovePi = require('node-grovepi').GrovePi
 var Commands = GrovePi.commands
 var Board = GrovePi.board
 
-var LightAnalogSensor = GrovePi.sensors.LightAnalog
+var LightAnalogSensor = GrovePi.sensors.LightAnalog;
+var DHTDigitalSensor = GrovePi.sensors.DHTDigital;
+var DigitalSensor = GrovePi.sensors.base.Digital;
+var AnalogSensor = GrovePi.sensors.base.Analog;
 
 var board = new Board({
     debug: true,
@@ -15,12 +18,39 @@ var board = new Board({
       if (res) {
         console.log('GrovePi Version :: ' + board.version())
 
-        var lightSensor = new LightAnalogSensor(2)
+        var lightSensor = new LightAnalogSensor(2);
+        var DHTSensor = new DHTDigitalSensor(8);
+        var Button = new DigitalSensor(2);
+        var touch = new DigitalSensor(3);
+
         console.log('Light Analog Sensor (start watch)')
+
+        //light
         lightSensor.on('change', function(res) {
-          console.log('Light onChange value=' + res)
+          console.log('Light onChange value=' + res + "\n");
         })
         lightSensor.watch()
+        
+        //dht11
+        DHTSensor.on('change', function(res) {
+            console.log('temperaturee onChange value=' + res[0] + "\n");
+            console.log('humidity onChange value=' + res[1] + "\n");
+            temperature_value = res[0];
+            humidity_value = res[1];
+        })
+        DHTSensor.watch()
+        
+        //button
+        Button.on('change', function(res) {
+            console.log('Button onChange value=' + res + "\n");
+        })
+        Button.watch()
+
+        //touch
+        touch.on('change', function(res) {
+            console.log('touch onChange value=' + res + "\n");
+        })
+        touch.watch()
       }
     }
   })
